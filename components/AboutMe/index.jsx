9a3d285fda0b9c538de5a857cf278b1ddeabe23a
best-code <cpmraygun@gmail.com>
2022-12-03 18:16:51 -0500

    Using next/image

--- components/AboutMe/index.jsx
+++ components/AboutMe/index.jsx
@@ -1,32 +1,36 @@
+import Image from "next/image";
+
 const AboutMe = () => {
     return (
         <div id="AboutMe" className="sm:pt-12 md:pt-18 lg:pt-24 bg-gray-800">
-        <div className="sm:pb-12 sm:px-12 md:pb-18 md:px-18 lg:pb-20 lg:px-20 bg-gray-800">
-            <div className="flex xl:flex-row flex-col w-full shadow-lg bg-white">
-                {/* Picture */}
-                <div className="xl:w-1/2 h-[16em] md:h-[24em] lg:h-[32em] overflow-hidden">
-                    <img width="100%" src="/faceshot.jpg" alt="Picture of Colin Maloney" />
-                </div>
-                {/*Picture End */}
+            <div className="sm:pb-12 sm:px-12 md:pb-18 md:px-18 lg:pb-20 lg:px-20 bg-gray-800">
+                <div className="flex xl:flex-row flex-col w-full shadow-lg bg-white">
+                    {/* Picture */}
+                    <div style={{width: '100%', height: '100%', position: 'relative'}}>
+                        <div className="h-[35em] overflow-hidden bg-red-900">
+                            <Image layout={'fill'} src={"/faceshot.jpg"} alt={"Picture of Colin Maloney"} />
+                        </div>
+                    </div>
+                    {/*Picture End */}
 
-                <div className="flex flex-col gap-0 xl:w-1/2 px-4 lg:px-12 py-8 font-iceland">
-                    <span className="text-5xl">
-                        About Me
-                    </span>
-                    <div className="text-4xl">
-                        <span>
-                            Computer Science B.S. Major at Florida State University
-                            with a passion for programming.
+                    <div className="flex flex-col gap-0 w-1/2 px-4 py-8 font-iceland">
+                        <span className="text-5xl">
+                            About Me
                         </span>
-                        <br /><br /><br />
-                        <span>
-                            Interested in Web Development,
-                            Game Development, Data Science, and Machine Learning.
-                        </span>
+                        <div className="text-4xl">
+                            <span>
+                                Computer Science B.S. Major at Florida State University
+                                with a passion for programming.
+                            </span>
+                            <br /><br /><br />
+                            <span>
+                                Interested in Web Development,
+                                Game Development, Data Science, and Machine Learning.
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
      //   </div>
     )
 }
